9b4ca719c3641415fa98570834f4db0d7a8bd50c
0xAA55 <weixiaobao213@qq.com>
2026-03-07 18:07:51 +0800

    应用 stub，并将 gl33.asm 加入链接。

--- build.bat
+++ build.bat
@@ -5,6 +5,7 @@ tools\nasm -f win32 main.asm -o out\main.obj
 tools\nasm -f win32 strcmp.asm -o out\strcmp.obj
 tools\nasm -f win32 stricmp.asm -o out\stricmp.obj
 tools\nasm -f win32 loaddll.asm -o out\loaddll.obj
-tools\link /ENTRY:start /LARGEADDRESSAWARE /NODEFAULTLIB /RELEASE /SUBSYSTEM:WINDOWS /OUT:ex2000.exe out\main.obj out\strcmp.obj out\stricmp.obj out\loaddll.obj
+tools\nasm -f win32 gl33.asm -o out\gl33.obj
+tools\link /ENTRY:start /LARGEADDRESSAWARE /NODEFAULTLIB /RELEASE /SUBSYSTEM:WINDOWS /STUB:out\stub.bin /OUT:ex2000.exe out\main.obj out\strcmp.obj out\stricmp.obj out\loaddll.obj out\gl33.obj
 
 pause
