bba69a19b943481bd799e6cbfbfd7332b9ca3490
0xAA55 <weixiaobao213@qq.com>
2026-03-08 01:46:39 +0800

    使输出调试信息以便于调试。

--- build.bat
+++ build.bat
@@ -6,6 +6,6 @@ tools\nasm -f win32 strcmp.asm -o out\strcmp.obj
 tools\nasm -f win32 stricmp.asm -o out\stricmp.obj
 tools\nasm -f win32 loaddll.asm -o out\loaddll.obj
 tools\nasm -f win32 gl33.asm -o out\gl33.obj
-tools\link /ENTRY:start /LARGEADDRESSAWARE /NODEFAULTLIB /RELEASE /SUBSYSTEM:WINDOWS /STUB:out\stub.bin /OUT:ex2000.exe out\main.obj out\strcmp.obj out\stricmp.obj out\loaddll.obj out\gl33.obj
+tools\link /NOLOGO /ENTRY:start /INCREMENTAL:NO /WS:AGGRESSIVE /LARGEADDRESSAWARE /NODEFAULTLIB /DEBUG /SUBSYSTEM:WINDOWS /STUB:out\stub.bin /OUT:ex2000.exe out\main.obj out\strcmp.obj out\stricmp.obj out\loaddll.obj out\gl33.obj
 
 pause
