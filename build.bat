085c3c10db8ad9217e3eeb157b8bf536037f1e16
0xAA55 <weixiaobao213@qq.com>
2026-03-08 01:50:30 +0800

    精简用不到的源码。

--- build.bat
+++ build.bat
@@ -2,8 +2,6 @@
 
 tools\nasm stub.asm -o out\stub.bin
 tools\nasm -f win32 main.asm -o out\main.obj
-tools\nasm -f win32 strcmp.asm -o out\strcmp.obj
-tools\nasm -f win32 stricmp.asm -o out\stricmp.obj
 tools\nasm -f win32 loaddll.asm -o out\loaddll.obj
 tools\nasm -f win32 gl33.asm -o out\gl33.obj
 tools\link /NOLOGO /ENTRY:start /INCREMENTAL:NO /WS:AGGRESSIVE /LARGEADDRESSAWARE /NODEFAULTLIB /DEBUG /SUBSYSTEM:WINDOWS /STUB:out\stub.bin /OUT:ex2000.exe out\main.obj out\strcmp.obj out\stricmp.obj out\loaddll.obj out\gl33.obj
