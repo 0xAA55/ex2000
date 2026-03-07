248983fc791d8a7761c94c8211ef7363ef2462f6
0xAA55 <weixiaobao213@qq.com>
2026-03-07 18:06:46 +0800

    测试调用 `MessageBoxA` 看看能不能用。

--- main.asm
+++ main.asm
@@ -1,12 +1,26 @@
 %include "frame.inc"
+%include "loaddll.inc"
 
 global _start
 extern _InitLoadLibrary
+segment .rdata
+_test_text db "test", 0
 
 segment .text
 _start:
 	call _InitLoadLibrary
 
+	load_dll User32
+	load_dll_func User32, MessageBoxA
 
+
+	push dword 0
+	push _test_text
+	push _test_text
+	push dword 0
+	invoke_dll_func MessageBoxA
 
 	ret
+
+def_dll User32, "user32.dll"
+def_dll_func MessageBoxA, "MessageBoxA"
