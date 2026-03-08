71e616134044da7b7b041a92a7900fec17d4ef93
0xAA55 <weixiaobao213@qq.com>
2026-03-09 00:57:33 +0800

    将不再参与编译的 OBJ 剔除。

--- build.bat
+++ build.bat
@@ -4,6 +4,6 @@ tools\nasm stub.asm -o out\stub.bin
 tools\nasm -f win32 main.asm -o out\main.obj
 tools\nasm -f win32 loaddll.asm -o out\loaddll.obj
 tools\nasm -f win32 gl33.asm -o out\gl33.obj
-tools\link /NOLOGO /ENTRY:start /INCREMENTAL:NO /WS:AGGRESSIVE /LARGEADDRESSAWARE /NODEFAULTLIB /DEBUG /SUBSYSTEM:WINDOWS /STUB:out\stub.bin /OUT:ex2000.exe out\main.obj out\strcmp.obj out\stricmp.obj out\loaddll.obj out\gl33.obj
+tools\link /NOLOGO /ENTRY:start /INCREMENTAL:NO /WS:AGGRESSIVE /LARGEADDRESSAWARE /NODEFAULTLIB /DEBUG /SUBSYSTEM:WINDOWS /STUB:out\stub.bin /OUT:ex2000.exe out\main.obj out\loaddll.obj out\gl33.obj
 
 pause
