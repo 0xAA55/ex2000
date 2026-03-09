20e76c15610605dc3a969e8746c64761d6e5d00c
0xAA55 <weixiaobao213@qq.com>
2026-03-10 01:26:14 +0800

    将新的模块加入链接。

--- build.bat
+++ build.bat
@@ -4,7 +4,8 @@ tools\nasm stub.asm -o out\stub.bin
 tools\nasm -f win32 main.asm -o out\main.obj
 tools\nasm -f win32 loaddll.asm -o out\loaddll.obj
 tools\nasm -f win32 gl33.asm -o out\gl33.obj
-tools\link /NOLOGO /ENTRY:start /INCREMENTAL:NO /WS:AGGRESSIVE /LARGEADDRESSAWARE /NODEFAULTLIB /DEBUG /SUBSYSTEM:WINDOWS /STUB:out\stub.bin /OUT:ex2000.exe out\main.obj out\loaddll.obj out\gl33.obj
+tools\nasm -f win32 scene.asm -o out\scene.obj
 tools\nasm -f win32 timer.asm -o out\timer.obj
+tools\link /NOLOGO /ENTRY:start /INCREMENTAL:NO /WS:AGGRESSIVE /LARGEADDRESSAWARE /NODEFAULTLIB /DEBUG /SUBSYSTEM:WINDOWS /STUB:out\stub.bin /OUT:ex2000.exe out\main.obj out\loaddll.obj out\gl33.obj out\scene.obj out\timer.obj
 
 pause
