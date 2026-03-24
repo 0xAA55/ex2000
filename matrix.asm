f6d458cf870fe313eb026cf5b73e2441d4be9111
0xAA55 <weixiaobao213@qq.com>
2026-03-25 02:34:30 +0800

    实现矩阵相乘。

--- matrix.asm
+++ matrix.asm
@@ -506,3 +506,30 @@ DefFunc _MatrixTranspose
 	FrameEnd
 	ret
 
+DefFunc _MatrixMultiply
+	FrameBegin 0, 3, esi, edi
+
+	LoadParam edi, 0
+	LoadParam esi, 1
+
+	invoke_cdecl _VectorMultMatrix, [edi + Matrix.x], [esi + Matrix.x], Param(2)
+	invoke_cdecl _VectorMultMatrix, [edi + Matrix.y], [esi + Matrix.y], Param(2)
+	invoke_cdecl _VectorMultMatrix, [edi + Matrix.z], [esi + Matrix.z], Param(2)
+	invoke_cdecl _VectorMultMatrix, [edi + Matrix.w], [esi + Matrix.w], Param(2)
+
+	FrameEnd
+	ret
+
+DefFunc _MatrixMultiplyTransposed
+	FrameBegin 0, 3, esi, edi
+
+	LoadParam edi, 0
+	LoadParam esi, 1
+
+	invoke_cdecl _VectorMultMatrixTransposed, [edi + Matrix.x], [esi + Matrix.x], Param(2)
+	invoke_cdecl _VectorMultMatrixTransposed, [edi + Matrix.y], [esi + Matrix.y], Param(2)
+	invoke_cdecl _VectorMultMatrixTransposed, [edi + Matrix.z], [esi + Matrix.z], Param(2)
+	invoke_cdecl _VectorMultMatrixTransposed, [edi + Matrix.w], [esi + Matrix.w], Param(2)
+
+	FrameEnd
+	ret
