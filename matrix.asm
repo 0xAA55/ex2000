9b4980715200618b32b775ad667586441f0e21ab
0xAA55 <weixiaobao213@qq.com>
2026-03-27 23:59:04 +0800

    实现在地址相关参数的前面加上 C 语言风格的取地址符号，可以使宏利用 `lea` 指令来传递参数。

--- matrix.asm
+++ matrix.asm
@@ -326,10 +326,10 @@ DefFunc _MatrixMultiply
 	LoadParam edi, 0
 	LoadParam esi, 1
 
-	invoke_cdecl _VectorMultMatrix, [edi + Matrix.x], [esi + Matrix.x], Param(2)
-	invoke_cdecl _VectorMultMatrix, [edi + Matrix.y], [esi + Matrix.y], Param(2)
-	invoke_cdecl _VectorMultMatrix, [edi + Matrix.z], [esi + Matrix.z], Param(2)
-	invoke_cdecl _VectorMultMatrix, [edi + Matrix.w], [esi + Matrix.w], Param(2)
+	invoke_cdecl _VectorMultMatrix, &[edi + Matrix.x], &[esi + Matrix.x], Param(2)
+	invoke_cdecl _VectorMultMatrix, &[edi + Matrix.y], &[esi + Matrix.y], Param(2)
+	invoke_cdecl _VectorMultMatrix, &[edi + Matrix.z], &[esi + Matrix.z], Param(2)
+	invoke_cdecl _VectorMultMatrix, &[edi + Matrix.w], &[esi + Matrix.w], Param(2)
 
 	FrameEnd
 	ret
@@ -340,10 +340,10 @@ DefFunc _MatrixMultiplyTransposed
 	LoadParam edi, 0
 	LoadParam esi, 1
 
-	invoke_cdecl _VectorMultMatrixTransposed, [edi + Matrix.x], [esi + Matrix.x], Param(2)
-	invoke_cdecl _VectorMultMatrixTransposed, [edi + Matrix.y], [esi + Matrix.y], Param(2)
-	invoke_cdecl _VectorMultMatrixTransposed, [edi + Matrix.z], [esi + Matrix.z], Param(2)
-	invoke_cdecl _VectorMultMatrixTransposed, [edi + Matrix.w], [esi + Matrix.w], Param(2)
+	invoke_cdecl _VectorMultMatrixTransposed, &[edi + Matrix.x], &[esi + Matrix.x], Param(2)
+	invoke_cdecl _VectorMultMatrixTransposed, &[edi + Matrix.y], &[esi + Matrix.y], Param(2)
+	invoke_cdecl _VectorMultMatrixTransposed, &[edi + Matrix.z], &[esi + Matrix.z], Param(2)
+	invoke_cdecl _VectorMultMatrixTransposed, &[edi + Matrix.w], &[esi + Matrix.w], Param(2)
 
 	FrameEnd
 	ret
