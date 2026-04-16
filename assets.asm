cbc3acf8f246034a4f35ed94c7a1cb8cf8fd018f
0xAA55 <weixiaobao213@qq.com>
2026-04-16 19:26:58 +0800

    准备实现资料库，使用 CAB 压缩格式存储需要的数据。

---
+++ assets.asm
@@ -0,0 +1,26 @@
+%include "avlbst.inc"
+%define ASSETS_ASM 1
+%include "assets.inc"
+
+dll_func_group_start CabinetFunc
+def_dll_func _FDICreate
+def_dll_func _FDICopy
+def_dll_func _FDIDestroy
+dll_func_group_end CabinetFunc
+
+segment .text
+global _AssetsInit
+_AssetsInit:
+	FrameBegin 0, 0
+	def_dll_and_load Cabinet, "cabinet.dll"
+	dll_func_group_load Cabinet, CabinetFunc
+	FrameEnd
+	ret
+
+
+segment .rdata
+global _AssetsCab
+_AssetsCab:
+incbin "out/assets.cab"
+_AssetsCabSize equ $ - _AssetsCab
+
