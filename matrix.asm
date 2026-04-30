65aa1a6e211de305bbd7343190ba06d0772ac745
0xAA55 <weixiaobao213@qq.com>
2026-04-30 14:36:09 +0800

    新增函数 `VectorCross()`

--- matrix.asm
+++ matrix.asm
@@ -16,6 +16,30 @@ global _ZeroVector
 _ZeroVector resd 4
 
 segment .text
+DefFunc _VectorCross
+	FrameBegin 0, 0
+
+	mov eax, Param(1)
+	mov ecx, Param(2)
+	mov edx, Param(0)
+
+	movaps xmm0, [eax]
+	movaps xmm1, xmm0
+	movaps xmm2, [ecx]
+	movaps xmm3, xmm2
+	shufps xmm0, xmm0, _MM_SHUFFLE(3, 0, 2, 1)
+	shufps xmm1, xmm1, _MM_SHUFFLE(3, 1, 0, 2)
+	shufps xmm2, xmm2, _MM_SHUFFLE(3, 1, 0, 2)
+	shufps xmm3, xmm3, _MM_SHUFFLE(3, 0, 2, 1)
+	mulps xmm0, xmm2
+	mulps xmm1, xmm3
+	subps xmm0, xmm1
+
+	movaps [edx], xmm0
+
+	FrameEnd
+	ret
+
 DefFunc _VectorMultMatrix
 	FrameBegin 0, 0
 
