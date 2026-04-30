912a1b49b53f5f5062b511b13f26e92dee89dafc
0xAA55 <weixiaobao213@qq.com>
2026-04-30 19:34:56 +0800

    消除用不到的 `MatrixRotationX` `MatrixRotationY` `MatrixRotationZ`

--- matrix.asm
+++ matrix.asm
@@ -146,18 +146,6 @@ DefFunc _VectorMultMatrixTransposed
 	FrameEnd
 	ret
 
-DefFunc _PreMatRot
-	fld dword [eax]
-	fld st0
-	fcos
-	fst dword [edx + 0]
-	fsin
-	fld st0
-	fst dword [edx + 4]
-	fchs
-	fst dword [edx + 8]
-	ret
-
 DefFunc _MatrixIdentity
 	FrameBegin 0, 0, edi
 
@@ -175,72 +163,6 @@ DefFunc _MatrixIdentity
 	mov [edi + Matrix.yy], eax
 	mov [edi + Matrix.zz], eax
 	mov [edi + Matrix.ww], eax
-
-	FrameEnd
-	ret
-
-DefFunc _MatrixRotationX
-	FrameBegin 3, 1
-
-	invoke_cdecl _MatrixIdentity, Param(0)
-
-	lea eax, Param(1)
-	lea edx, Variable(0)
-	call _PreMatRot
-
-	LoadParam eax, 0
-	mov ecx, Variable(0)
-	mov edx, Variable(1)
-
-	mov [eax + Matrix.yy], ecx
-	mov [eax + Matrix.yz], edx
-	mov [eax + Matrix.zz], ecx
-	mov edx, Variable(2)
-	mov [eax + Matrix.zy], edx
-
-	FrameEnd
-	ret
-
-DefFunc _MatrixRotationY
-	FrameBegin 3, 1
-
-	invoke_cdecl _MatrixIdentity, Param(0)
-
-	lea eax, Param(1)
-	lea edx, Variable(0)
-	call _PreMatRot
-
-	mov eax, Param(0)
-	mov ecx, Variable(0)
-	mov edx, Variable(1)
-
-	mov [eax + Matrix.xx], ecx
-	mov [eax + Matrix.zx], edx
-	mov [eax + Matrix.zz], ecx
-	mov edx, Variable(2)
-	mov [eax + Matrix.xz], edx
-
-	FrameEnd
-	ret
-
-DefFunc _MatrixRotationZ
-	FrameBegin 3, 1
-
-	invoke_cdecl _MatrixIdentity, Param(0)
-
-	lea eax, Param(1)
-	lea edx, Variable(0)
-	call _PreMatRot
-
-	mov eax, Param(0)
-	mov ecx, Variable(0)
-	mov edx, Variable(1)
-
-	mov [eax + Matrix.xx], ecx
-	mov [eax + Matrix.xy], edx
-	mov [eax + Matrix.yy], ecx
-	mov edx, Variable(2)
-	mov [eax + Matrix.yx], edx
 
 	FrameEnd
 	ret
