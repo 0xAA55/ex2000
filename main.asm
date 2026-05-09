bc422441ed3be82541b4237811fc6869e970a721
0xAA55 <weixiaobao213@qq.com>
2026-05-09 19:28:12 +0800

    移除不需要的函数引入。

--- main.asm
+++ main.asm
@@ -110,8 +110,6 @@ def_dll_func vsnprintf
 def_dll_func memset
 def_dll_func memcpy
 def_dll_func memmove
-def_dll_func cos
-def_dll_func sin
 def_dll_func rand
 def_dll_func srand
 dll_func_group_end CFunc
