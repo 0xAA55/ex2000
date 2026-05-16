87897a4d208fec09a287123ebc89ceb6c855ccb5
0xAA55 <weixiaobao213@qq.com>
2026-05-16 23:48:19 +0800

    尝试实现 `FloatMapKMapGen()`：使用线程池，计算一个图像的光追步长参数图。

--- matrix.asm
+++ matrix.asm
@@ -1733,16 +1733,90 @@ DefFunc _FloatMapGetMaxValue
 	%undef _RET
 	%undef _TMP
 
-DefFunc _FloatMapKMapGen
-	FrameBegin 0, 3, ebx, esi
+DefFunc _FloatMapKMapGenProc
+	FrameBegin 7, 3, ebx, esi, edi
+	AssignVars _X, _WY, _WM, _HF, _DRP, _MAX, _DMAP
 
-	mov eax, Param(0)
-	invoke_cdecl _GenDistanceMap, [eax + FloatMap.border_len]
+	mov ebx, Param(0)
+	mov esi, [ebx + FMDataCmn.src_map]
+	mov edi, [ebx + FMDataCmn.dst_map]
+	mov eax, [ebx + FMDataCmn.userdata]
+	mov eax, [eax + FloatMap.data]
+	mov _DMAP, eax
+
+	mov eax, [esi + FloatMap.border_len]
+	shr eax, 1
+	mov _HF, eax
+
+	mov eax, Param(1)
+	add eax, _HF
+	mov _WY, eax
+
+	mov eax, Param(1)
+	mov eax, [edi + FloatMap.row_ptr + eax * 4]
+	mov _DRP, eax
+
+	xor eax, eax
+	mov _X, eax
+.loopx:
+	add eax, _HF
+	invoke_cdecl _WarpFloatMap, esi, eax, _WY
 	mov ebx, eax
 
+	mov eax, [ebx + FloatMap.data]
+	mov edx, _DMAP
+	mov ecx, [ebx + FloatMap.num_pixels]
+	test cl, 0xF
+	jz .vector_process
+.single_process:
+	movss xmm0, [eax + (ecx - 1) * 4]
+	divss xmm0, [edx + (ecx - 1) * 4]
+	movss [eax + (ecx - 1) * 4], xmm0
+	loop .single_process
+	jmp .process_end
+.vector_process:
+	shr ecx, 4
+.vector_loop:
+	movaps xmm0, [eax + 0x00]
+	movaps xmm1, [eax + 0x10]
+	movaps xmm2, [eax + 0x20]
+	movaps xmm3, [eax + 0x30]
+	mulss xmm0, [edx + 0x00]
+	mulss xmm1, [edx + 0x10]
+	mulss xmm2, [edx + 0x20]
+	mulss xmm3, [edx + 0x30]
+	movaps [eax + 0x00], xmm0
+	movaps [eax + 0x10], xmm1
+	movaps [eax + 0x20], xmm2
+	movaps [eax + 0x30], xmm3
+	add eax, 0x40
+	add edx, 0x40
+	loop .vector_loop
 
-
+.process_end:
+	invoke_cdecl _FloatMapGetMaxValue, ebx
+	mov _MAX, eax
 	invoke_cdecl _DestroyFloatMap, ebx
+	mov eax, _X
+	mov ecx, _MAX
+	mov edx, _DRP
+	mov [edx + eax * 4], ecx
+	inc eax
+	mov _X, eax
+	cmp eax, [esi + FloatMap.border_len]
+	jb .loopx
 
+	FrameEnd
+	ret
+
+DefFunc _FloatMapKMapGen
+	FrameBegin 1, 5, ebx, esi
+	mov eax, Param(0)
+	invoke_cdecl _GenDistanceMap, [eax + FloatMap.border_len]
+	mov esi, eax
+	invoke_cdecl _FloatMapMTPool, Param(0), 8, esi, _FloatMapKMapGenProc, 0
+	mov ebx, eax
+	invoke_cdecl _DestroyFloatMap, esi
+	mov eax, ebx
 	FrameEnd
 	ret
