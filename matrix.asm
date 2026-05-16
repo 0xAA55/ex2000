4e62e3fa78c7008f06f25b50457ea7b1e95af418
0xAA55 <weixiaobao213@qq.com>
2026-05-16 23:45:50 +0800

    实现 `FloatMapApplyGain()`：可以对一个位图施加增益值。

--- matrix.asm
+++ matrix.asm
@@ -1207,6 +1207,46 @@ DefFunc _MultiplyFloatMap
 	movss [edi + eax], xmm0
 	add eax, 4
 	loop .tail
+.end:
+	FrameEnd
+	ret
+
+DefFunc _FloatMapApplyGain
+	FrameBegin 0, 0, ebx, esi
+
+	mov ebx, Param(0)
+
+	mov eax, [ebx + FloatMap.num_pixels]
+	mul dword[ebx + FloatMap.dims]
+	mov esi, [ebx + FloatMap.data]
+	movss xmm7, Param(1)
+	test al, 0xF
+	mov ecx, eax
+	jz .batch_proc
+.single_proc:
+	movss xmm0, [esi + (ecx - 1) * 4]
+	mulss xmm0, xmm7
+	movss [esi + (ecx - 1) * 4], xmm0
+	loop .single_proc
+	jmp .end
+.batch_proc:
+	shr ecx, 4
+	shufps xmm7, xmm7, 0
+.proc:
+	movaps xmm0, [esi + 0x00]
+	movaps xmm1, [esi + 0x10]
+	movaps xmm2, [esi + 0x20]
+	movaps xmm3, [esi + 0x30]
+	mulps xmm0, xmm7
+	mulps xmm1, xmm7
+	mulps xmm2, xmm7
+	mulps xmm3, xmm7
+	movaps [esi + 0x00], xmm0
+	movaps [esi + 0x10], xmm1
+	movaps [esi + 0x20], xmm2
+	movaps [esi + 0x30], xmm3
+	add esi, 0x40
+	loop .proc
 
 .end:
 	FrameEnd
