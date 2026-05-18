fd33ef7937c5654151a4a4f1c88e90c393db0b14
0xAA55 <weixiaobao213@qq.com>
2026-05-18 16:46:31 +0800

    XP 的 `msvcrt.dll` 没有 `vsnprintf()`，但是有 `_vsnprintf()`

--- loaddll.asm
+++ loaddll.asm
@@ -69,7 +69,7 @@ def_dll_func strcpy
 def_dll_func strcat
 def_dll_func strlen
 def_dll_func strcmp
-def_dll_func vsnprintf
+def_dll_func_alias vsnprintf, "_vsnprintf"
 def_dll_func memset
 def_dll_func memcpy
 def_dll_func memmove
