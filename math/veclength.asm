79eb5a49f4beb924762f8c8e5f18b316d87863b6
0xAA55 <weixiaobao213@qq.com>
2026-05-19 21:09:41 +0800

    采用贪婪获取向量 + 使用位域屏蔽非维度数据 + 强行四维向量点乘后求平方根的方式，进行无循环求向量摸。

--- math/veclength.asm
+++ math/veclength.asm
@@ -7,15 +7,26 @@ DefFunc _VectorLength
 	mov eax, Param(1)
 	mov ecx, Param(2)
 	mov edx, Param(0)
-	fldz
-.muladd:
-	fld dword [eax]
-	fmul dword [eax]
-	fadd
-	add eax, 4
-	loop .muladd
-	fsqrt
-	fstp dword [edx]
+	dec ecx
+	shl ecx, 4
+
+	movups xmm0, [eax]
+	pand xmm0, [_UF000 + ecx]
+	mulps xmm0, xmm0
+	cmp dword [_HaveSSE3], 0
+	jz .no_sse3
+	haddps xmm0, xmm0
+	haddps xmm0, xmm0
+	jmp .after_dot
+.no_sse3:
+	movhlps xmm1, xmm0
+	movaps xmm2, xmm0
+	addps xmm2, xmm1
+	pshufd xmm0, xmm2, _MM_SHUFFLE(1, 1, 1, 1)
+	addss xmm0, xmm2
+.after_dot:
+	sqrtss xmm0
+	movss [edx], xmm0
 
 	FrameEnd
 	ret
