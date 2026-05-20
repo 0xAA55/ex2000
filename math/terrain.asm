b0e44c7e289c0f1c4e38b3949a7de9f845f444eb
0xAA55 <weixiaobao213@qq.com>
2026-05-20 20:21:32 +0800

    使可以生成完整的地形顶点和索引数据。

--- math/terrain.asm
+++ math/terrain.asm
@@ -44,10 +44,8 @@ DefFunc _AltitudeToTerrain
 .gen_indices:
 	%define _T1 %[_NUM_V]
 	%define _T2 %[_NUM_I]
-	%define _T3 %[_CB_V]
 	%undef _NUM_V
 	%undef _NUM_I
-	%undef _CB_V
 
 	xor eax, eax
 	mov edi, [ebx + SimpleMesh.indices]
@@ -92,6 +90,13 @@ DefFunc _AltitudeToTerrain
 	jb .loopy_i
 
 .gen_vertices:
+	%define _CUR_ROW %[_T1]
+	%define _PREV_ROW %[_T2]
+	%define _NEXT_ROW %[_CB_V]
+	%undef _T1
+	%undef _T2
+	%undef _CB_V
+
 	mov edi, [ebx + SimpleMesh.vertices]
 	cvtsi2ss xmm5, [esi + FloatMap.border_len]
 	movss xmm4, Param(2)
@@ -105,51 +110,48 @@ DefFunc _AltitudeToTerrain
 	xor eax, eax
 	mov _Y, eax
 .loopy:
-	mov ecx, eax
-	mov edx, eax
-	dec ecx
-	inc edx
+	lea ecx, [eax - 1]
+	lea edx, [eax + 1]
 	and ecx, _BM
 	and edx, _BM
 	mov eax, [esi + FloatMap.row_ptr + eax * 4]
 	mov ecx, [esi + FloatMap.row_ptr + ecx * 4]
 	mov edx, [esi + FloatMap.row_ptr + edx * 4]
-	movss xmm0, [ecx]
-	subss xmm0, [edx]
-	mulss xmm0, xmm5 ;nz
-	movss _T1, xmm0
-	mov _T2, eax
+	mov _CUR_ROW, eax
+	mov _PREV_ROW, ecx
+	mov _NEXT_ROW, edx
 	xor eax, eax
 	mov _X, eax
 .loopx:
-	mov ecx, eax
-	mov edx, eax
-	dec ecx
-	inc edx
-	mov eax, _T2
+	mov ecx, _PREV_ROW
+	mov edx, _NEXT_ROW
+	lea ecx, [ecx + eax * 4]
+	lea edx, [edx + eax * 4]
+	movss xmm2, [ecx]
+	subss xmm2, [edx]
+	mulss xmm2, xmm5 ;nz
+	lea ecx, [eax - 1]
+	lea edx, [eax + 1]
+	lea eax, [eax * 4]
+	add eax, _CUR_ROW
+	movss xmm1, [eax]
+	mov eax, _CUR_ROW
 	and ecx, _BM
 	and edx, _BM
-	mov ecx, [eax + ecx * 4]
-	mov edx, [eax + edx * 4]
+	lea ecx, [eax + ecx * 4]
+	lea edx, [eax + edx * 4]
 	movss xmm0, [ecx]
+	mulss xmm1, Param(1)
 	subss xmm0, [edx]
+	movss [edi + SimpleVertex.y], xmm1
 	mulss xmm0, xmm5 ;nx
-	
-	mov eax, _T1
-	mov [edi + Vertex.nz], eax
-	mov dword[edi + Vertex.ny], 0x3F800000
-	movss [edi + Vertex.nx], xmm0
 
-	lea eax, [edi + Vertex.nx]
+	movss [edi + SimpleVertex.nx], xmm0
+	mov dword[edi + SimpleVertex.ny], 0x3F800000
+	movss [edi + SimpleVertex.nz], xmm1
+
+	lea eax, [edi + SimpleVertex.nx]
 	invoke_cdecl _VectorNormal, eax, eax, 3
-
-	mov eax, _X
-	mov ecx, Vertex.size
-	mul ecx
-	add eax, _T2
-	movss xmm0, [eax]
-	mulss xmm0, Param(1)
-	movss [edi + Vertex.y], xmm0
 
 	movq xmm0, _X
 	cvtdq2ps xmm0, xmm0
@@ -157,9 +159,9 @@ DefFunc _AltitudeToTerrain
 	mulps xmm0, xmm4
 	mulps xmm1, xmm6
 	pshufd xmm2, xmm0, _MM_SHUFFLE(1, 1, 1, 1)
-	movss [edi + Vertex.x], xmm0
-	movss [edi + Vertex.z], xmm2
-	movq [edi + Vertex.u], xmm1
+	movss [edi + SimpleVertex.x], xmm0
+	movss [edi + SimpleVertex.z], xmm2
+	movq [edi + SimpleVertex.u], xmm1
 	add edi, ecx
 
 	mov eax, _X
@@ -179,3 +181,9 @@ DefFunc _AltitudeToTerrain
 	ret
 	%undef _X
 	%undef _Y
+	%undef _CUR_ROW
+	%undef _PREV_ROW
+	%undef _NEXT_ROW
+	%undef _BM
+	%undef _YI
+	%undef _YI2
