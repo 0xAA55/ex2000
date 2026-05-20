3e5cbdb1ce6f2c4643fd423c214abe6ddb9bd0a7
0xAA55 <weixiaobao213@qq.com>
2026-05-20 23:30:50 +0800

    因为有向量点乘的函数了，所以向量求模的函数得以简化。

--- math/veclength.asm
+++ math/veclength.asm
@@ -2,31 +2,13 @@
 
 segment .text
 DefFunc _VectorLength
-	FrameBegin 0, 0
+	FrameBegin 0, 4, ebx
 
-	mov eax, Param(1)
-	mov ecx, Param(2)
-	mov edx, Param(0)
-	dec ecx
-	shl ecx, 4
-
-	movups xmm0, [eax]
-	pand xmm0, [_UF000 + ecx]
-	mulps xmm0, xmm0
-	cmp dword [_HaveSSE3], 0
-	jz .no_sse3
-	haddps xmm0, xmm0
-	haddps xmm0, xmm0
-	jmp .after_dot
-.no_sse3:
-	movhlps xmm1, xmm0
-	movaps xmm2, xmm0
-	addps xmm2, xmm1
-	pshufd xmm0, xmm2, _MM_SHUFFLE(1, 1, 1, 1)
-	addss xmm0, xmm2
-.after_dot:
+	mov ebx, Param(0)
+	invoke_cdecl _VectorDot, Param(0), Param(1), Param(1), Param(2)
+	movss xmm0, [ebx]
 	sqrtss xmm0, xmm0
-	movss [edx], xmm0
+	movss [ebx], xmm0
 
 	FrameEnd
 	ret
