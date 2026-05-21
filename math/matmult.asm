3d3402bb523e9325c48441295bbefdaae9dec0ea
0xAA55 <weixiaobao213@qq.com>
2026-05-22 00:39:06 +0800

    实现进行矩阵乘法时，直接将乘的结果存入左矩阵。会自动分配栈上内存用于存储中间结果。

--- math/matmult.asm
+++ math/matmult.asm
@@ -12,5 +12,27 @@ DefFunc _MatrixMultiply
 	invoke_cdecl _VectorMultMatrix, &[edi + Matrix.z], &[esi + Matrix.z], Param(2)
 	invoke_cdecl _VectorMultMatrix, &[edi + Matrix.w], &[esi + Matrix.w], Param(2)
 
+
+	FrameEnd
+	ret
+
+DefFunc _MatrixMultiplyTo
+	FrameBegin 0x14, 3, ebx, edi
+
+	lea ebx, Variable(4)
+	mov edi, Param(0)
+	and ebx, 0xFFFFFFF0
+
+	invoke_cdecl _MatrixMultiply, ebx, edi, Param(1)
+
+	movaps xmm0, [ebx + Matrix.x]
+	movaps xmm1, [ebx + Matrix.y]
+	movaps xmm2, [ebx + Matrix.z]
+	movaps xmm3, [ebx + Matrix.w]
+	movaps [edi + Matrix.x], xmm0
+	movaps [edi + Matrix.y], xmm1
+	movaps [edi + Matrix.z], xmm2
+	movaps [edi + Matrix.w], xmm3
+
 	FrameEnd
 	ret
