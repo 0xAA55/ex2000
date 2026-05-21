b855007070f2bc206a72980f84903943ae75b8be
0xAA55 <weixiaobao213@qq.com>
2026-05-22 00:45:32 +0800

    参考 `glFrustum()` 实现 Z 值从 -1 到正增长。

--- math/matproj.asm
+++ math/matproj.asm
@@ -11,7 +11,8 @@ DefFunc _MatrixProjection
 	movaps [eax + Matrix.z], xmm0
 	movaps [eax + Matrix.w], xmm0
 
-	mov dword [eax + Matrix.zw], __?float32?__(1.0)
+	movss xmm3, Param(3)
+	movss xmm4, Param(4)
 
 	fld dword Param(1)
 	fdiv dword [_2.0f]
@@ -21,15 +22,18 @@ DefFunc _MatrixProjection
 	fdiv dword Param(2)
 	fstp dword [eax + Matrix.xx]
 
-	movss xmm0, Param(4)
-	movss xmm3, Param(4)
-	subss xmm3, Param(3)
-	divss xmm0, xmm3
-	movss [eax + Matrix.zz], xmm0
-	movss xmm1, [_ZeroVector]
-	subss xmm1, Param(3)
-	mulss xmm1, xmm0
-	movss [eax + Matrix.wz], xmm1
+	movss xmm0, xmm3
+	movss xmm1, xmm3
+	movss xmm2, xmm3
+	subss xmm0, xmm4 ;zn - zf
+	addss xmm1, xmm4 ;zn + zf
+	mulss xmm2, xmm4
+	mulss xmm2, [_2.0f] ;2znzf
+	divss xmm1, xmm0 ;(zn + zf) / (zn - zf)
+	divss xmm2, xmm0 ;2znzf / (zn - zf)
+	movss [eax + Matrix.zz], xmm1
+	movss [eax + Matrix.zw], xmm2
+	mov dword [eax + Matrix.wz], __?float32?__(-1.0)
 
 	FrameEnd
 	ret
