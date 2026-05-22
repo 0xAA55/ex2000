b7a07ae81a8236feeab4788a21d5e229527682ae
0xAA55 <weixiaobao213@qq.com>
2026-05-22 20:33:04 +0800

    参考 `glFrustum()` 和 `D3DXMatrixPerspectiveFovLH()` 实现行主序透视投影矩阵。

--- math/matproj.asm
+++ math/matproj.asm
@@ -11,29 +11,30 @@ DefFunc _MatrixProjection
 	movaps [eax + Matrix.z], xmm0
 	movaps [eax + Matrix.w], xmm0
 
-	movss xmm3, Param(3)
-	movss xmm4, Param(4)
+	movss xmm6, Param(3)
+	movss xmm7, Param(4)
+	movss xmm1, [_M1.0f]
+	movss xmm2, [_M2.0f]
 
 	fld dword Param(1)
-	fdiv dword [_2.0f]
 	fsincos
 	fdivp
-	fst dword [eax + Matrix.yy]
-	fdiv dword Param(2)
-	fstp dword [eax + Matrix.xx]
+	fst dword [eax + Matrix.xx]
+	fmul dword Param(2)
+	fstp dword [eax + Matrix.yy]
 
-	movss xmm0, xmm3
-	movss xmm1, xmm3
-	movss xmm2, xmm3
-	subss xmm0, xmm4 ;zn - zf
-	addss xmm1, xmm4 ;zn + zf
-	mulss xmm2, xmm4
-	mulss xmm2, [_2.0f] ;2znzf
-	divss xmm1, xmm0 ;(zn + zf) / (zn - zf)
-	divss xmm2, xmm0 ;2znzf / (zn - zf)
-	movss [eax + Matrix.zz], xmm1
+	movss xmm0, xmm7
+	movss xmm3, xmm7
+	mulss xmm2, xmm6
+	mulss xmm0, xmm1
+	subss xmm3, xmm6 ; Depth
+	mulss xmm2, xmm7
+	divss xmm0, xmm3
+	movss xmm7, xmm7
+	divss xmm2, xmm3
+	movss [eax + Matrix.zz], xmm0
 	movss [eax + Matrix.zw], xmm2
-	mov dword [eax + Matrix.wz], __?float32?__(-1.0)
+	movss [eax + Matrix.wz], xmm1
 
 	FrameEnd
 	ret
