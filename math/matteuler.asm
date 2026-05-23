bbd354284241193b74cb3e292b32a275dd658bc0
0xAA55 <weixiaobao213@qq.com>
2026-05-23 19:00:05 +0800

    将函数 `MatrixTransformPositionEuler()` 名字改为 `MatrixEulerTranslated()`

--- math/matteuler.asm
+++ math/matteuler.asm
@@ -1,15 +1,18 @@
 %include "common.inc"
 
 segment .text
-DefFunc _MatrixTransformPositionEuler
-	FrameBegin 0, 4
+DefFunc _MatrixEulerTranslated
+	FrameBegin 0, 4, edi
 
-	invoke_cdecl _MatrixRotationEuler, Param(0), Param(2), Param(3), Param(4)
-	mov eax, Param(0)
+	mov edi, Param(0)
+	invoke_cdecl _MatrixRotationEuler, edi, Param(2), Param(3), Param(4)
 	mov ecx, Param(1)
+	jecxz .end
 	movaps xmm0, [ecx]
-	movaps [eax + Matrix.w], xmm0
-	mov dword [eax + Matrix.ww], 0x3F800000
+	pand xmm0, [_UFFF0]
+	addps xmm0, [edi + Matrix.w]
+	movaps [edi + Matrix.w], xmm0
 
+.end:
 	FrameEnd
 	ret
