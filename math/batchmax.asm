907cd05fb5cdc5a73e4d34b0f99fdca499b060c7
0xAA55 <weixiaobao213@qq.com>
2026-05-27 23:59:59 +0800

    无规约结果写出中间缓冲区域，全部规约到栈上以避免内存分配。

--- math/batchmax.asm
+++ math/batchmax.asm
@@ -1,31 +1,27 @@
 %include "common.inc"
+%include "avlbst.inc"
 
 DefFunc _BatchMax
-	FrameBegin 1, 3, ebx, esi, edi
-	AssignVars _RET
+	FrameBegin 8, 3, ebx, esi, edi
 
-	mov esi, Param(0)
+	lea edi, Variable(4)
+	and edi, 0xFFFFFFF0
+
+	mov ebx, Param(0)
 	mov eax, Param(1)
 
 	test eax, eax ; param sanity check
-	jnz .begin_process
-	mov dword [ebx], 0xFF800000 ;-1.INF
+	jnz .good
+	mov dword[edi], 0xFF800000
 	jmp .result
-.begin_process:
-	shl eax, 2
-	mov edi, eax
-	invoke_cdecl _aligned_malloc, edi, 16 ;allcate the intermediate buffer for fast reducing parallel max
-	mov ebx, eax
-	invoke_dll_cdecl memcpy, ebx, esi, edi
+.good:
+	movaps xmm0, [ebx]
+	movaps [edi], xmm0
 	mov eax, Param(1)
 .on_choose_methods: ; to trim data or to batch process data
 	mov esi, ebx
 	test al, 0x3F
 	jz .batch_64
-	test al, 0x1F
-	jz .batch_32
-	test al, 0xF
-	jz .batch_16
 	test al, 0x7
 	jz .batch_8
 	mov ecx, eax ; trim from back
@@ -40,9 +36,8 @@ DefFunc _BatchMax
 	jz .result
 	jmp .on_choose_methods
 .batch_64:
-	mov ecx, eax ; batch process data: every 64 input, one 4 output to the intermediate buffer
+	mov ecx, eax ; batch process data: every 64 input
 	shr ecx, 6
-	xor eax, eax
 .loop_64_to_4:
 	movaps xmm0, [esi + 0x00]
 	movaps xmm1, [esi + 0x20]
@@ -64,70 +59,33 @@ DefFunc _BatchMax
 	maxps xmm2, xmm3
 	maxps xmm4, xmm5
 	maxps xmm6, xmm7
+	movaps xmm1, [edi]
 	maxps xmm0, xmm2
 	maxps xmm4, xmm6
 	maxps xmm0, xmm4
-	movaps [ebx + eax * 4], xmm0
+	maxps xmm0, xmm1
+	movaps [edi], xmm0
 	add esi, 0x100
-	add eax, 4
-	loop .loop_64_to_4
-	jmp .on_choose_methods
-.batch_32:
-	mov ecx, eax ; batch process data: every 16 input, one 4 output to the intermediate buffer
-	shr ecx, 5
-	xor eax, eax
-.loop_32_to_4:
-	movaps xmm0, [esi + 0x00]
-	movaps xmm1, [esi + 0x20]
-	movaps xmm2, [esi + 0x40]
-	movaps xmm3, [esi + 0x60]
-	maxps xmm0, [esi + 0x10]
-	maxps xmm1, [esi + 0x30]
-	maxps xmm2, [esi + 0x50]
-	maxps xmm3, [esi + 0x70]
-	maxps xmm0, xmm2
-	maxps xmm1, xmm3
-	maxps xmm0, xmm1
-	movaps [ebx + eax * 4], xmm0
-	add esi, 0x80
-	add eax, 4
-	loop .loop_32_to_4
-	jmp .on_choose_methods
-.batch_16:
-	mov ecx, eax ; batch process data: every 16 input, one 4 output to the intermediate buffer
-	shr ecx, 4
-	xor eax, eax
-.loop_16_to_4:
-	movaps xmm0, [esi + 0x00]
-	movaps xmm1, [esi + 0x20]
-	maxps xmm0, [esi + 0x10]
-	maxps xmm1, [esi + 0x30]
-	maxps xmm0, xmm1
-	movaps [ebx + eax * 4], xmm0
-	add esi, 0x40
-	add eax, 4
-	loop .loop_16_to_4
+	dec ecx
+	jnz .loop_64_to_4
+	mov eax, 4
 	jmp .on_choose_methods
 .batch_8:
 	mov ecx, eax ; batch process data: every 8 input, one 4 output to the intermediate buffer
 	shr ecx, 3
-	xor eax, eax
+	movaps xmm0, [edi]
 .loop_8_to_4:
-	movaps xmm0, [esi + 0x00]
+	maxps xmm0, [esi + 0x00]
 	maxps xmm0, [esi + 0x10]
-	movaps [ebx + eax * 4], xmm0
 	add esi, 0x20
-	add eax, 4
 	loop .loop_8_to_4
+	movaps [edi], xmm0
+	mov eax, 4
 	jmp .on_choose_methods
 
 .result:
-	movss xmm0, [ebx]
-	movss _RET, xmm0
-	invoke_cdecl _aligned_free, ebx
-	fld dword _RET
+	fld dword [edi]
 
 .end:
 	FrameEnd
 	ret
-	%undef _RET
