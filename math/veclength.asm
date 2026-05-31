b6c269ca67b018da0c8d26abd60e465f055fde84
0xAA55 <weixiaobao213@qq.com>
2026-05-31 20:14:31 +0800

    直接使用非易失寄存器传参。

--- math/veclength.asm
+++ math/veclength.asm
@@ -4,7 +4,7 @@ DefFunc _VectorLength
 	FrameBegin 0, 4, ebx
 
 	mov ebx, Param(0)
-	invoke_cdecl _VectorDot, Param(0), Param(1), Param(1), Param(2)
+	invoke_cdecl _VectorDot, ebx, Param(1), Param(1), Param(2)
 	movss xmm0, [ebx]
 	sqrtss xmm0, xmm0
 	movss [ebx], xmm0
