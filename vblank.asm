8a03cedc9489eea633cdfb71cb8baa46a1a55a72
0xAA55 <weixiaobao213@qq.com>
2026-06-27 23:12:36 +0800

    去掉多余的逻辑，只用 `dxgi.dll` 实现枚举显卡和显卡上的口，获取所有的显示器，并实现让窗口在对应显示器上的时候，按照该显示器的刷新率进行垂直同步等待。

--- vblank.asm
+++ vblank.asm
@@ -8,41 +8,7 @@ extern _hDC
 def_dll DXGI, "dxgi.dll"
 def_dll_func CreateDXGIFactory
 
-def_dll D3D11, "d3d11.dll"
-def_dll_func D3D11CreateDevice
-
 %define D3D11_SDK_VERSION 7
-
-%define D3D_DRIVER_TYPE_UNKNOWN 0
-%define D3D_DRIVER_TYPE_HARDWARE 1
-%define D3D_DRIVER_TYPE_REFERENCE 2
-%define D3D_DRIVER_TYPE_NULL 3
-%define D3D_DRIVER_TYPE_SOFTWARE 4
-%define D3D_DRIVER_TYPE_WARP 5
-
-%define MAX_OUTPUTS_PER_ADAPTER 32
-%define MAX_ADAPTERS 32
-
-struc ID3D11DeviceVtbl
-	.QueryInterface resd 1
-	.AddRef resd 1
-	.Release resd 1
-endstruc
-
-struc IDXGIDeviceVtbl
-	.QueryInterface resd 1
-	.AddRef resd 1
-	.Release resd 1
-	.SetPrivateData resd 1
-	.SetPrivateDataInterface resd 1
-	.GetPrivateData resd 1
-	.GetParent resd 1
-	.GetAdapter resd 1
-	.CreateSurface resd 1
-	.QueryResourceResidency resd 1
-	.SetGPUThreadPriority resd 1
-	.GetGPUThreadPriority resd 1
-endstruc
 
 struc IDXGIFactoryVtbl
 	.QueryInterface resd 1
@@ -100,17 +66,10 @@ struc DXGI_OUTPUT_DESC
 	.AttachedToDesktop resd 1
 	.Rotation resd 1
 	.HMonitor resd 1
-	.Dummy resd 1 ;This dummy must be initialized to zero, so `.HMonitor` could be used as a string key because a string key needs a NUL char.
 	.size equ $ - .DeviceName
 endstruc
 
 segment .rdata
-extern _IID_IDXGIDevice
-_IID_IDXGIDevice:
-	dd 0x54ec77fa
-	dw 0x1377, 0x44e6
-	db 0x8c, 0x32, 0x88, 0xfd, 0x5f, 0x44, 0xc8, 0x4c
-
 extern _IID_IDXGIFactory
 _IID_IDXGIFactory:
 	dd 0x7b7166ec
@@ -126,8 +85,8 @@ extern _DXGIOutputs
 _DXGIOutputs resd 1
 
 DefFunc _VBlankInit
-	FrameBegin 5 + DXGI_OUTPUT_DESC.size / 4, ebx, esi, edi
-	AssignVars DXGIFactory, D3D11Device, DXGIDevice, DXGIOutput, DXGIAdapter, DXGIOutputDesc
+	FrameBegin 8 + DXGI_OUTPUT_DESC.size / 4, ebx, esi, edi
+	AssignVars DXGIFactory, D3D11Device, DXGIDevice, DXGIOutput, DXGIAdapter, HMonitor, Dummy, DXGIOutputDesc
 
 	xor eax, eax
 	lea edi, DXGIFactory
@@ -139,91 +98,47 @@ DefFunc _VBlankInit
 
 	load_dll DXGI
 	test eax, eax
-	jz .enum_monitors_by_d3d11
+	jz .no_dxgi
 
 	load_dll_func DXGI, CreateDXGIFactory
 	test eax, eax
-	jz .enum_monitors_by_d3d11
+	jz .no_dxgi
 
 	invoke_dll_stdcall CreateDXGIFactory, _IID_IDXGIFactory, &DXGIFactory
 	cmp eax, 0
-	jl .enum_monitors_by_d3d11
+	jl .no_dxgi
 
 .loop_enum_monitors_by_dxgi:
 	invoke_com DXGIFactory, IDXGIFactoryVtbl.EnumAdapters, esi, &DXGIAdapter
 	cmp eax, 0
-	jl .enum_next_adapter_dxgi
+	jl .end_enum_adapeters_dxgi
 
 	xor edi, edi
 .loop_enum_outputs_dxgi:
 	invoke_com DXGIAdapter, IDXGIAdapterVtbl.EnumOutputs, edi, &DXGIOutput
 	cmp eax, 0
-	jl .enum_next_output_dxgi
+	jl .end_enum_outputs_dxgi
 	invoke_com DXGIOutput, IDXGIOutputVtbl.GetDesc, ebx
-	invoke_cdecl _AVLInsert, _DXGIOutputs, &[ebx + DXGI_OUTPUT_DESC.HMonitor], DXGIOutput, _ReleaseComObj
-
-.enum_next_output_dxgi:
+	mov eax, [ebx + DXGI_OUTPUT_DESC.HMonitor]
+	mov HMonitor, eax ; Use `Dummy` as `NUL` for `strcmp()` inside `AVLInsert()`
+	invoke_cdecl _AVLInsert, _DXGIOutputs, &HMonitor, DXGIOutput, _ReleaseComObj
 	inc edi
-	cmp edi, MAX_OUTPUTS_PER_ADAPTER
-	jb .loop_enum_outputs_dxgi
+	jmp .loop_enum_outputs_dxgi
 
+.end_enum_outputs_dxgi:
 	invoke_cdecl _SafeRelease, &DXGIAdapter
-
-.enum_next_adapter_dxgi
 	inc esi
-	cmp esi, MAX_ADAPTERS
-	jb .loop_enum_monitors_by_dxgi
+	jmp .loop_enum_monitors_by_dxgi
 
+.end_enum_adapeters_dxgi:
 	invoke_cdecl _SafeRelease, &DXGIFactory
-
-	jmp .end_enum_outputs
-.enum_monitors_by_d3d11:
-	load_dll D3D11
-	test eax, eax
-	jz .no_d3d11
-
-	load_dll_func D3D11, D3D11CreateDevice
-	test eax, eax
-	jz .no_d3d11
-
-	invoke_dll_stdcall D3D11CreateDevice, NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, 0, NULL, 0, D3D11_SDK_VERSION, &D3D11Device, NULL, NULL
-	cmp eax, 0
-	jl .no_d3d11
-
-	invoke_com D3D11Device, ID3D11DeviceVtbl.QueryInterface, _IID_IDXGIDevice, &DXGIDevice
-	cmp eax, 0
-	jl .d3d11_initfail
-	invoke_cdecl _SafeRelease, &D3D11Device
-
-	invoke_com DXGIDevice, IDXGIDeviceVtbl.GetAdapter, &DXGIAdapter
-	cmp eax, 0
-	jl .d3d11_initfail
-	invoke_cdecl _SafeRelease, &DXGIDevice
-
-.loop_enum_outputs_d3d11:
-	invoke_com DXGIAdapter, IDXGIAdapterVtbl.EnumOutputs, edi, &DXGIOutput
-	cmp eax, 0
-	jl .enum_next_output_d3d11
-	invoke_com DXGIOutput, IDXGIOutputVtbl.GetDesc, ebx
-	invoke_cdecl _AVLInsert, _DXGIOutputs, &[ebx + DXGI_OUTPUT_DESC.HMonitor], DXGIOutput, _ReleaseComObj
-
-.enum_next_output_d3d11:
-	inc edi
-	cmp edi, MAX_OUTPUTS_PER_ADAPTER
-	jb .loop_enum_outputs_d3d11
-
-	invoke_cdecl _SafeRelease, &DXGIAdapter
-
-.end_enum_outputs:
 	mov dword[_addr_of_WaitForVBlank], _WaitForVBlankD3D
 
 	xor eax, eax
 	inc eax
 	jmp .end
-.d3d11_initfail:
-	invoke_cdecl _VBlankD3DDeInit
 
-.no_d3d11:
+.no_dxgi:
 	; Fallback to `_FakeWaitForVBlank`
 
 .end:
@@ -234,6 +149,8 @@ DefFunc _VBlankInit
 	%undef DXGIDevice
 	%undef DXGIOutput
 	%undef DXGIAdapter
+	%undef HMonitor
+	%undef Dummy
 	%undef DXGIOutputDesc
 
 DefFunc _WaitForVBlankD3D
@@ -246,7 +163,7 @@ DefFunc _WaitForVBlankD3D
 	stosd
 
 	invoke_dll_stdcall MonitorFromWindow, [_hWnd], MONITOR_DEFAULTTONEAREST
-	mov HMonitor, eax
+	mov HMonitor, eax ; Use `Dummy` as `NUL` for `strcmp()` inside `AVLInsert()`
 	invoke_cdecl _AVLSearch, [_DXGIOutputs], &HMonitor
 	test eax, eax
 	jz .not_found
@@ -254,28 +171,20 @@ DefFunc _WaitForVBlankD3D
 	invoke_com [eax + AVLBST_Node.userdata], IDXGIOutputVtbl.WaitForVBlank
 	jmp .end
 .not_found:
-	invoke_dll_stdcall Sleep, 1
+	invoke_cdecl _FakeWaitForVBlank
 
 .end:
 	FrameEnd
 	ret
 
-DefFunc _VBlankD3DDeInit
-	FrameBegin 0, ebx
-
-	invoke_cdecl _AVLClear, _DXGIOutputs
-
-	FrameEnd
-	ret
-
 DefFunc _VBlankDeInit
 	FrameBegin 0
-	invoke_cdecl _VBlankD3DDeInit
+	invoke_cdecl _AVLClear, _DXGIOutputs
 	FrameEnd
 	ret
 
 DefFunc _FakeWaitForVBlank
 	FrameBegin 0
-	invoke_dll_stdcall Sleep, 1
+	invoke_dll_stdcall Sleep, 100 ; Debug, will change to 1
 	FrameEnd
 	ret
