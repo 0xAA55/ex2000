5f6f97184a5dab7da6a727077fbf8e03f16ca414
0xAA55 <weixiaobao213@qq.com>
2026-07-06 14:47:11 +0800

    实现 `FloatMapGetMinValue()`

---
+++ math/floatmapmin.asm
@@ -0,0 +1,17 @@
+%include "common.inc"
+
+DefFunc _FloatMapGetMinValue
+	FrameBegin 0, ebx
+
+	mov ebx, Param(0)
+	mov eax, [ebx + BitMap.num_floats]
+	test eax, eax
+	jnz .good
+.bad:
+	int3
+	jmp .bad
+.good:
+	invoke_cdecl _BatchMin, [ebx + BitMap.data], eax
+
+	FrameEnd
+	ret
