f43f37a6cf9ef4a0ac90b7eec6a9393854471821
0xAA55 <weixiaobao213@qq.com>
2026-07-07 21:23:47 +0800

    获取精确到微秒的屏幕刷新率数值，并使用高精度的睡眠函数进行延迟的渲染，实现最强跟手性优化。

--- vblank.asm
+++ vblank.asm
@@ -72,10 +72,21 @@ struc DXGI_OUTPUT_DESC
 	.AttachedToDesktop resd 1
 	.Rotation resd 1
 	.HMonitor resd 1
-	.size equ $ - .DeviceName
+	.size equ $ - DXGI_OUTPUT_DESC
 endstruc
 
-struc IDirectDraw
+struc DXGI_MODE_DESC
+	.Width resd 1
+	.Height resd 1
+	.RefreshRate_Numerator resd 1
+	.RefreshRate_Denominator resd 1
+	.Format resd 1
+	.ScanlineOrdering resd 1
+	.Scaling resd 1
+	.size equ $ - DXGI_MODE_DESC
+endstruc
+
+struc IDirectDrawVtbl
 	.QueryInterface resd 1
 	.AddRef resd 1
 	.Release resd 1
@@ -107,8 +118,110 @@ struc MonitorData
 	.IsVRR resd 1
 	.RefreshRate resd 1
 	.RefreshIntervalMs resd 1
+	.RefreshIntervalUs resd 1
 	.size equ $ - MonitorData
 endstruc
+
+%define DXGI_FORMAT_UNKNOWN	                    0
+%define DXGI_FORMAT_R32G32B32A32_TYPELESS       1
+%define DXGI_FORMAT_R32G32B32A32_FLOAT          2
+%define DXGI_FORMAT_R32G32B32A32_UINT           3
+%define DXGI_FORMAT_R32G32B32A32_SINT           4
+%define DXGI_FORMAT_R32G32B32_TYPELESS          5
+%define DXGI_FORMAT_R32G32B32_FLOAT             6
+%define DXGI_FORMAT_R32G32B32_UINT              7
+%define DXGI_FORMAT_R32G32B32_SINT              8
+%define DXGI_FORMAT_R16G16B16A16_TYPELESS       9
+%define DXGI_FORMAT_R16G16B16A16_FLOAT          10
+%define DXGI_FORMAT_R16G16B16A16_UNORM          11
+%define DXGI_FORMAT_R16G16B16A16_UINT           12
+%define DXGI_FORMAT_R16G16B16A16_SNORM          13
+%define DXGI_FORMAT_R16G16B16A16_SINT           14
+%define DXGI_FORMAT_R32G32_TYPELESS             15
+%define DXGI_FORMAT_R32G32_FLOAT                16
+%define DXGI_FORMAT_R32G32_UINT                 17
+%define DXGI_FORMAT_R32G32_SINT                 18
+%define DXGI_FORMAT_R32G8X24_TYPELESS           19
+%define DXGI_FORMAT_D32_FLOAT_S8X24_UINT        20
+%define DXGI_FORMAT_R32_FLOAT_X8X24_TYPELESS    21
+%define DXGI_FORMAT_X32_TYPELESS_G8X24_UINT     22
+%define DXGI_FORMAT_R10G10B10A2_TYPELESS        23
+%define DXGI_FORMAT_R10G10B10A2_UNORM           24
+%define DXGI_FORMAT_R10G10B10A2_UINT            25
+%define DXGI_FORMAT_R11G11B10_FLOAT             26
+%define DXGI_FORMAT_R8G8B8A8_TYPELESS           27
+%define DXGI_FORMAT_R8G8B8A8_UNORM              28
+%define DXGI_FORMAT_R8G8B8A8_UNORM_SRGB         29
+%define DXGI_FORMAT_R8G8B8A8_UINT               30
+%define DXGI_FORMAT_R8G8B8A8_SNORM              31
+%define DXGI_FORMAT_R8G8B8A8_SINT               32
+%define DXGI_FORMAT_R16G16_TYPELESS             33
+%define DXGI_FORMAT_R16G16_FLOAT                34
+%define DXGI_FORMAT_R16G16_UNORM                35
+%define DXGI_FORMAT_R16G16_UINT                 36
+%define DXGI_FORMAT_R16G16_SNORM                37
+%define DXGI_FORMAT_R16G16_SINT                 38
+%define DXGI_FORMAT_R32_TYPELESS                39
+%define DXGI_FORMAT_D32_FLOAT                   40
+%define DXGI_FORMAT_R32_FLOAT                   41
+%define DXGI_FORMAT_R32_UINT                    42
+%define DXGI_FORMAT_R32_SINT                    43
+%define DXGI_FORMAT_R24G8_TYPELESS              44
+%define DXGI_FORMAT_D24_UNORM_S8_UINT           45
+%define DXGI_FORMAT_R24_UNORM_X8_TYPELESS       46
+%define DXGI_FORMAT_X24_TYPELESS_G8_UINT        47
+%define DXGI_FORMAT_R8G8_TYPELESS               48
+%define DXGI_FORMAT_R8G8_UNORM                  49
+%define DXGI_FORMAT_R8G8_UINT                   50
+%define DXGI_FORMAT_R8G8_SNORM                  51
+%define DXGI_FORMAT_R8G8_SINT                   52
+%define DXGI_FORMAT_R16_TYPELESS                53
+%define DXGI_FORMAT_R16_FLOAT                   54
+%define DXGI_FORMAT_D16_UNORM                   55
+%define DXGI_FORMAT_R16_UNORM                   56
+%define DXGI_FORMAT_R16_UINT                    57
+%define DXGI_FORMAT_R16_SNORM                   58
+%define DXGI_FORMAT_R16_SINT                    59
+%define DXGI_FORMAT_R8_TYPELESS                 60
+%define DXGI_FORMAT_R8_UNORM                    61
+%define DXGI_FORMAT_R8_UINT                     62
+%define DXGI_FORMAT_R8_SNORM                    63
+%define DXGI_FORMAT_R8_SINT                     64
+%define DXGI_FORMAT_A8_UNORM                    65
+%define DXGI_FORMAT_R1_UNORM                    66
+%define DXGI_FORMAT_R9G9B9E5_SHAREDEXP          67
+%define DXGI_FORMAT_R8G8_B8G8_UNORM             68
+%define DXGI_FORMAT_G8R8_G8B8_UNORM             69
+%define DXGI_FORMAT_BC1_TYPELESS                70
+%define DXGI_FORMAT_BC1_UNORM                   71
+%define DXGI_FORMAT_BC1_UNORM_SRGB              72
+%define DXGI_FORMAT_BC2_TYPELESS                73
+%define DXGI_FORMAT_BC2_UNORM                   74
+%define DXGI_FORMAT_BC2_UNORM_SRGB              75
+%define DXGI_FORMAT_BC3_TYPELESS                76
+%define DXGI_FORMAT_BC3_UNORM                   77
+%define DXGI_FORMAT_BC3_UNORM_SRGB              78
+%define DXGI_FORMAT_BC4_TYPELESS                79
+%define DXGI_FORMAT_BC4_UNORM                   80
+%define DXGI_FORMAT_BC4_SNORM                   81
+%define DXGI_FORMAT_BC5_TYPELESS                82
+%define DXGI_FORMAT_BC5_UNORM                   83
+%define DXGI_FORMAT_BC5_SNORM                   84
+%define DXGI_FORMAT_B5G6R5_UNORM                85
+%define DXGI_FORMAT_B5G5R5A1_UNORM              86
+%define DXGI_FORMAT_B8G8R8A8_UNORM              87
+%define DXGI_FORMAT_B8G8R8X8_UNORM              88
+%define DXGI_FORMAT_R10G10B10_XR_BIAS_A2_UNORM  89
+%define DXGI_FORMAT_B8G8R8A8_TYPELESS           90
+%define DXGI_FORMAT_B8G8R8A8_UNORM_SRGB         91
+%define DXGI_FORMAT_B8G8R8X8_TYPELESS           92
+%define DXGI_FORMAT_B8G8R8X8_UNORM_SRGB         93
+%define DXGI_FORMAT_BC6H_TYPELESS               94
+%define DXGI_FORMAT_BC6H_UF16                   95
+%define DXGI_FORMAT_BC6H_SF16                   96
+%define DXGI_FORMAT_BC7_TYPELESS                97
+%define DXGI_FORMAT_BC7_UNORM                   98
+%define DXGI_FORMAT_BC7_UNORM_SRGB              99
 
 %define DDENUM_ATTACHEDSECONDARYDEVICES 0x00000001
 %define DDENUM_DETACHEDSECONDARYDEVICES 0x00000002
@@ -137,13 +250,19 @@ _VBlankTimer:
 	InstTimer
 
 extern _VBlankTimeUsedMs
+extern _VBlankTimeUsedUs
 _VBlankTimeUsedMs resd 1
+_VBlankTimeUsedUs resd 1
 
 extern _LastFrameRenderTimeMs
+extern _LastFrameRenderTimeUs
 _LastFrameRenderTimeMs resd 1
+_LastFrameRenderTimeUs resd 1
 
 extern _FrameRenderDelayMs
+extern _FrameRenderDelayUs
 _FrameRenderDelayMs resd 1
+_FrameRenderDelayUs resd 1
 
 extern _VBlankFrameStartTime
 _VBlankFrameStartTime resq 1
@@ -295,13 +414,15 @@ DefFunc _DDEnumCallbackExA@20
 	ret 20
 
 DefFunc _SetupMonitorDataProc
-	FrameBegin SizedVar(MONITORINFOEXW.size) + SizedVar(DEVMODEW.size), ebx, edi
+	FrameBegin SizedVar(MONITORINFOEXW.size) + SizedVar(DEVMODEW.size) + SizedVar(DXGI_MODE_DESC.size) + SizedVar(DXGI_MODE_DESC.size), ebx, esi, edi
 	AssignSizedVar _MonitorInfoExW, MONITORINFOEXW.size
 	AssignSizedVar _DevModeW, DEVMODEW.size
+	AssignSizedVar _DesiredMode, DXGI_MODE_DESC.size
+	AssignSizedVar _ClosestMode, DXGI_MODE_DESC.size
 
 	xor eax, eax
-	lea edi, _MonitorInfoExW
 	mov ecx, Frame_NumLocals
+	lea edi, &_MonitorInfoExW
 	rep stosd
 
 	mov ebx, Param(1)
@@ -309,6 +430,7 @@ DefFunc _SetupMonitorDataProc
 	mov byte[_DevModeW_Addr + DEVMODEW.dmSize], DEVMODEW.size
 	mov byte[ebx + MonitorData.RefreshRate], 60
 	mov byte[ebx + MonitorData.RefreshIntervalMs], 16
+	mov word[ebx + MonitorData.RefreshIntervalUs], 16666
 
 	invoke_dll_stdcall GetMonitorInfoW, Param(0), &_MonitorInfoExW
 	test eax, eax
@@ -317,12 +439,38 @@ DefFunc _SetupMonitorDataProc
 	test eax, eax
 	jz .end
 
+	mov eax, [_MonitorInfoExW_Addr + MONITORINFOEXW.rcMonitor_r]
+	mov ecx, [_MonitorInfoExW_Addr + MONITORINFOEXW.rcMonitor_b]
+	sub eax, [_MonitorInfoExW_Addr + MONITORINFOEXW.rcMonitor_x]
+	sub ecx, [_MonitorInfoExW_Addr + MONITORINFOEXW.rcMonitor_y]
+	mov [_DesiredMode_Addr + DXGI_MODE_DESC.Width], eax
+	mov [_DesiredMode_Addr + DXGI_MODE_DESC.Height], ecx
+	mov byte[_DesiredMode_Addr + DXGI_MODE_DESC.Format], DXGI_FORMAT_R8G8B8A8_UNORM
+
 	mov eax, 1000
 	xor edx, edx
 	mov ecx, [_DevModeW_Addr + DEVMODEW.dmDisplayFrequency]
 	div ecx
 	mov [ebx + MonitorData.RefreshRate], ecx
 	mov [ebx + MonitorData.RefreshIntervalMs], eax
+	mov ecx, 1000
+	mul ecx
+	mov [ebx + MonitorData.RefreshIntervalUs], eax
+
+	mov esi, [ebx + MonitorData.IDXGIOutput]
+	test esi, esi
+	jz .end
+
+	invoke_com esi, IDXGIOutputVtbl.FindClosestMatchingMode, &_DesiredMode, &_ClosestMode, NULL
+	cmp eax, 0
+	jl .end
+
+	mov eax, [_ClosestMode_Addr + DXGI_MODE_DESC.RefreshRate_Denominator]
+	mov ecx, [_ClosestMode_Addr + DXGI_MODE_DESC.RefreshRate_Numerator]
+	mov edx, 1000000
+	mul edx
+	div ecx
+	mov [ebx + MonitorData.RefreshIntervalUs], eax
 
 .end:
 	FrameEnd
@@ -331,6 +479,10 @@ DefFunc _SetupMonitorDataProc
 	%undef _MonitorInfoExW_Addr
 	%undef _DevModeW
 	%undef _DevModeW_Addr
+	%undef _DesiredMode
+	%undef _DesiredMode_Addr
+	%undef _ClosestMode
+	%undef _ClosestMode_Addr
 
 DefFunc _VBlankDeInit
 	FrameBegin 0
@@ -377,10 +529,12 @@ DefFunc _WaitForVBlank
 	fst qword _VBlankStartTimeL
 	fsub qword [_VBlankFrameStartTime]
 	fimul word [_WThousand]
-	fistp dword [_LastFrameRenderTimeMs]
+	fist dword [_LastFrameRenderTimeMs]
+	fimul word [_WThousand]
+	fistp dword [_LastFrameRenderTimeUs]
 
-	mov eax, [ebx + MonitorData.RefreshIntervalMs]
-	cmp [_LastFrameRenderTimeMs], eax
+	mov eax, [ebx + MonitorData.RefreshIntervalUs]
+	cmp [_LastFrameRenderTimeUs], eax
 	jae .overloaded
 
 .w_dxgi:
@@ -393,7 +547,7 @@ DefFunc _WaitForVBlank
 	mov eax, [ebx + MonitorData.IDirectDraw]
 	test eax, eax
 	jz .not_found
-	invoke_com eax, IDirectDraw.WaitForVerticalBlank, DDWAITVB_BLOCKEND, NULL
+	invoke_com eax, IDirectDrawVtbl.WaitForVerticalBlank, DDWAITVB_BLOCKEND, NULL
 .w_after:
 	cmp eax, 0
 	jl .not_found
@@ -402,14 +556,20 @@ DefFunc _WaitForVBlank
 	fst qword _NewFrameStartTimeL
 	fsub qword _VBlankStartTimeL
 	fimul word [_WThousand]
-	fistp dword [_VBlankTimeUsedMs]
+	fist dword [_VBlankTimeUsedMs]
+	fimul word [_WThousand]
+	fistp dword [_VBlankTimeUsedUs]
 
-	mov eax, [ebx + MonitorData.RefreshIntervalMs]
-	sub eax, [_LastFrameRenderTimeMs]
+	mov eax, [ebx + MonitorData.RefreshIntervalUs]
+	sub eax, [_LastFrameRenderTimeUs]
 	jbe .no_delay
-	dec eax
-	mov [_FrameRenderDelayMs], eax
-	invoke_cdecl _HybridWaitMs, eax
+	sub eax, 1000
+	jbe .no_delay
+	mov [_FrameRenderDelayUs], eax
+	invoke_cdecl _HybridWaitUs, eax, 0
+	fild dword[_FrameRenderDelayUs]
+	fidiv word[_WThousand]
+	fist dword[_FrameRenderDelayMs]
 
 	invoke_cdecl _UpdateTimer, _VBlankTimer
 	fstp qword [_VBlankFrameStartTime]
@@ -417,13 +577,16 @@ DefFunc _WaitForVBlank
 .no_delay:
 	movq xmm0, _NewFrameStartTimeL
 	movq [_VBlankFrameStartTime], xmm0
-	jmp .end
+	jmp .no_delay_set_vars
 .overloaded:
 	movq xmm0, _VBlankStartTimeL
 	movq [_VBlankFrameStartTime], xmm0
+.no_delay_set_vars:
 	xor eax, eax
 	mov [_VBlankTimeUsedMs], eax
+	mov [_VBlankTimeUsedUs], eax
 	mov [_FrameRenderDelayMs], eax
+	mov [_FrameRenderDelayUs], eax
 
 .not_found:
 .end:
