8a10b7fe3a35a013f325d64f7110a69f8cd25187
0xAA55 <weixiaobao213@qq.com>
2026-07-31 02:41:49 +0800

    添加测试代码，测试每次迭代时生成的 UV 图的差异。

--- math/conemap.asm
+++ math/conemap.asm
@@ -341,6 +341,20 @@ DefFunc _ConeMapGen
 	cmp ecx, Param(1)
 	jae .break_loop
 
+[segment .bss]
+	.filename_buf resb 128
+
+[segment .rdata]
+	.binf_type db 'wb', 0
+
+__SECT__
+	DefVars %$File
+	snprintf .filename_buf, 128, `uvmap_%03d.bin`, %$NumIter
+	invoke_dll_cdecl fopen, .filename_buf, .binf_type
+	mov %$File, eax
+	invoke_dll_cdecl fwrite, [esi + BitMap.data], [esi + BitMap.num_bytes], 1, eax
+	invoke_dll_cdecl fclose, %$File
+
 	debug_printf `num iter = %d, num updated = %d\n`, %$NumIter, %$Modified
 	xor eax, eax
 	cmp %$Modified, eax
@@ -351,6 +365,7 @@ DefFunc _ConeMapGen
 	invoke_cdecl _DestroyBitMap, esi
 	invoke_cdecl _DestroyBitMap, %$Comparer
 
+	;Debug part. TODO: Run debug and delete the following line.
 	VisualizeFloatMap1D edi, `testcone.bmp`
 
 	mov eax, edi
