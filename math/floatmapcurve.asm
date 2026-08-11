405d9c5ebffc36b9b24fced42f780df21f0d6e35
0xAA55 <weixiaobao213@qq.com>
2026-08-11 16:08:10 +0800

    使对位图进行曲线化拉伸压缩处理的函数的实现由 `PoolRun()` 的接口改变而简化，减少一次内存分配，并新增线程池尺寸的参数。

--- math/floatmapcurve.asm
+++ math/floatmapcurve.asm
@@ -1,22 +1,19 @@
 %include "common.inc"
 
 struc PoolProcParam
+.bitmap resd 1
 .curve_ptr resd 1
 .curve_points resd 1
 .size equ $ - PoolProcParam
 endstruc
 
-segment .bss
-extern _FloatMapCurveNumWorkers
-_FloatMapCurveNumWorkers resd 1
-
 DefFunc _FloatMapCurvePoolProc
 	FrameBegin ebx, esi, edi
+	NameParams %$Params, %$CommonData, %$Index
 
-	mov ebx, Param(0)
-	mov edi, Param(1)
-	mov eax, Param(2)
-	mov esi, [edi + BitMap.row_ptr + eax * 4]
+	mov ebx, %$CommonData
+	mov esi, %$Params
+	mov edi, [ebx + PoolProcParam.bitmap]
 
 	mov eax, [edi + BitMap.border_len]
 	mul dword[edi + BitMap.dims]
@@ -27,35 +24,18 @@ DefFunc _FloatMapCurvePoolProc
 	ret
 
 DefFunc _FloatMapCurve
-	FrameBegin ebx, esi, edi
+	FrameBegin ebx
+	NameParams %$Map, %$Curve, %$NumCurvePoints, %$ThreadPoolSize
 
-	mov ebx, Param(0)
-	mov eax, [ebx + BitMap.border_len]
-	shl eax, 2
-	mov edi, eax
-	add eax, PoolProcParam.size
-	invoke_cdecl _malloc, eax
-	mov esi, eax
-	add edi, eax
-	mov ecx, [ebx + BitMap.border_len]
-	mov eax, edi
-	mov edi, esi
-	rep stosd
+	mov ebx, %$Map
 
-	cmp dword[_FloatMapCurveNumWorkers], 0
-	jnz .proceed_to_work
-	mov eax, [_SystemInfo + SYSTEM_INFO.dwNumberOfProcessors]
-	shr eax, 1
-	mov [_FloatMapCurveNumWorkers], eax
-.proceed_to_work:
+	mov eax, %$ThreadPoolSize
+	mov ecx, [_SystemInfo + SYSTEM_INFO.dwNumberOfProcessors]
+	test eax, eax
+	cmovz eax, ecx
+	mov %$ThreadPoolSize, eax
 
-	mov eax, Param(1)
-	mov ecx, Param(2)
-	mov [edi + PoolProcParam.curve_ptr], eax
-	mov [edi + PoolProcParam.curve_points], ecx
-
-	invoke_cdecl _PoolRun, _FloatMapCurvePoolProc, ebx, [_FloatMapCurveNumWorkers], [ebx + BitMap.border_len], esi, 0, 0
-	invoke_cdecl _free, esi
+	invoke_cdecl _PoolRun, _FloatMapCurvePoolProc, & %$Map, %$ThreadPoolSize, [ebx + BitMap.border_len], &[ebx + BitMap.row_ptr], 0, 0
 
 	FrameEnd
 	ret
