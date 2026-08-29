d673e64e05ef2d5184991357aec548ac6de78ea0
0xAA55 <weixiaobao213@qq.com>
2026-08-30 01:43:07 +0800

    着色器的部分优化成按照资源路径从 AVL 树查找已编译完成的着色器单元，加载单元后链接成完整的着色器以优化着色器的编译速度。

--- shellcodes/shader.asm
+++ shellcodes/shader.asm
@@ -2,195 +2,248 @@
 %include "shader.inc"
 %include "gl33.inc"
 %include "assets.inc"
+%include "avlbst.inc"
 %include "strpool.inc"
 
-segment .rdata
+segment .bss
+_ShadersTree resd 1
+
 DefStr _STVertexShader, "Vertex"
 DefStr _STGeometryShader, "Geometry"
 DefStr _STFragmentShader, "Fragment"
 DefStr _ShaderCompileError, "Shader compilation error"
 DefStr _ShaderLinkageError, "Shader linkage error"
 
-; int ShaderCreate(int program, int shader_type, char *shader_code, char **pp_out_infolog)
-DefFunc _ShaderCreate
+; const char *GetShaderTypeString(int shader_type);
+DefFunc _GetShaderTypeString
 	FrameBegin
-	NameParams %$Program, %$ShaderType, %$ShaderCode, %$PPOutInfoLog
-	DefVars %$Shader, %$SourceLen, %$CompileStatus, %$InfoLogLen, %$InfoLogBuf
+	NameParams %$ShaderType
 
-	invoke_stdcall glCreateShader, %$ShaderType
-	mov %$Shader, eax
-
-	invoke_cdecl strlen, %$ShaderCode
-	mov %$SourceLen, eax
-
-	invoke_stdcall glShaderSource, %$Shader, 1, & %$ShaderCode, &%$SourceLen
-	invoke_stdcall glCompileShader, %$Shader
-	invoke_stdcall glGetShaderiv, %$Shader, GL_COMPILE_STATUS, &%$CompileStatus
-
-	mov eax, %$CompileStatus
-	test eax, eax
-	jnz .success
-
-	invoke_stdcall glGetShaderiv, %$Shader, GL_INFO_LOG_LENGTH, &%$InfoLogLen
-	mov eax, %$InfoLogLen
-	inc eax
-	invoke_cdecl _calloc, eax, 1
-	mov %$InfoLogBuf, eax
-
-	invoke_stdcall glGetShaderInfoLog, %$Shader, %$InfoLogLen, 0, eax
-	invoke_stdcall glDeleteShader, %$Shader
-	mov eax, %$PPOutInfoLog
-	mov edx, %$InfoLogBuf
-	mov [eax], edx
-
-.failexit:
-	xor eax, eax
-	jmp .end
-.success:
-	invoke_stdcall glAttachShader, %$Program, %$Shader
-	invoke_stdcall glDeleteShader, %$Shader
-	xor eax, eax
-	inc eax
-
-.end:
-	FrameEnd
-	ret
-
-; GLuint ProgramCreate(char *VertexShader, char *GeometryShader, char *FragmentShader);
-DefFunc _ProgramCreate
-	FrameBegin
-	NameParams %$VS, %$GS, %$FS
-	invoke_cdecl _ProgramCreateEx, GL_VERTEX_SHADER, %$VS, GL_GEOMETRY_SHADER, %$GS, GL_FRAGMENT_SHADER, %$FS,0
-	FrameEnd
-	ret
-
-; GLuint _ProgramCreateEx(int shader_type, const char *shader_source, ...);
-DefFunc _ProgramCreateEx
-	FrameBegin
-	NameParams %$Args
-	invoke_cdecl _ProgramCreateExV, & %$Args
-	FrameEnd
-	ret
-
-; GLuint _ProgramCreateExV(void *ap_list);
-DefFunc _ProgramCreateExV
-	FrameBegin ebx, esi, edi
-	NameParams %$APList
-	DefVars %$InfoLog, %$InfoLogLen, %$LinkStatus, %$ShaderType, %$ShaderTypeStr
-
-	xor eax, eax
-	mov ecx, %$Frame_NumLocals
-	lea edi, Variable(0)
-	rep stosd
-
-	invoke_stdcall glCreateProgram
-	mov ebx, eax
-
-	mov esi, %$APList
-.next_shader:
-	lodsd
-	test eax, eax
-	jz .proceed_link
-	mov %$ShaderType, eax
+	mov eax, %$ShaderType
 	cmp eax, GL_VERTEX_SHADER
 	jz .is_vs
 	cmp eax, GL_GEOMETRY_SHADER
 	jz .is_gs
 	cmp eax, GL_FRAGMENT_SHADER
 	jz .is_fs
-	GetAbsAddr ecx, _ShaderCompileError
-	debug_msg "%s: Unknown shader type %d", ecx, %$ShaderType
-	jmp .bad_end
+	xor eax, eax
+	jmp .end
 .is_vs:
-	GetAbsAddr ecx, _STVertexShader
-	jmp .load_shader
+	GetAbsAddr eax, _STVertexShader
+	jmp .end
 .is_gs:
-	GetAbsAddr ecx, _STGeometryShader
-	jmp .load_shader
+	GetAbsAddr eax, _STGeometryShader
+	jmp .end
 .is_fs:
-	GetAbsAddr ecx, _STFragmentShader
-.load_shader:
-	mov %$ShaderTypeStr, ecx
+	GetAbsAddr eax, _STFragmentShader
+.end:
+	FrameEnd
+	ret
+
+; int ShaderCreate(int shader_type, char *shader_code)
+DefFunc _ShaderCreate
+	FrameBegin ebx, esi
+	NameParams %$ShaderType, %$ShaderCode
+	DefVars %$SourceLen, %$CompileStatus, %$InfoLogLen
+
+	invoke_stdcall glCreateShader, %$ShaderType
+	mov esi, eax
+
+	invoke_cdecl strlen, %$ShaderCode
+	mov %$SourceLen, eax
+
+	invoke_stdcall glShaderSource, esi, 1, & %$ShaderCode, &%$SourceLen
+	invoke_stdcall glCompileShader, esi
+	invoke_stdcall glGetShaderiv, esi, GL_COMPILE_STATUS, &%$CompileStatus
+
+	mov eax, %$CompileStatus
+	test eax, eax
+	jnz .success
+
+	invoke_stdcall glGetShaderiv, esi, GL_INFO_LOG_LENGTH, &%$InfoLogLen
+	mov eax, %$InfoLogLen
+	invoke_cdecl _calloc, &[eax + 1], 1
+	mov ebx, eax
+
+	invoke_stdcall glGetShaderInfoLog, esi, %$InfoLogLen, 0, eax
+	invoke_stdcall glDeleteShader, esi
+
+	invoke_cdecl _GetShaderTypeString, %$ShaderType
+	GetAbsAddr ecx, _ShaderCompileError
+	debug_msg "%s: %s Shader: %s", ecx, eax, ebx
+	invoke_cdecl _free, ebx
+
+	xor eax, eax
+	jmp .end
+.success:
+	mov eax, esi
+
+.end:
+	FrameEnd
+	ret
+
+; GLuint _ProgramLink(int program);
+DefFunc _ProgramLink
+	FrameBegin ebx, esi
+	NameParams %$Program, %$PPOutInfoLog
+	DefVars %$InfoLogLen, %$LinkStatus
+
+	mov esi, %$Program
+	invoke_stdcall glLinkProgram, esi
+	invoke_stdcall glGetProgramiv, esi, GL_LINK_STATUS, &%$LinkStatus
+	cmp dword %$LinkStatus, 0
+	jnz .good_link
+
+	invoke_stdcall glGetProgramiv, esi, GL_INFO_LOG_LENGTH, &%$InfoLogLen
+	mov eax, %$InfoLogLen
+	invoke_cdecl _calloc, &[eax + 1], 1
+	mov ebx, eax
+
+	invoke_stdcall glGetProgramInfoLog, esi, %$InfoLogLen, &%$InfoLogLen, ebx
+	GetAbsAddr ecx, _ShaderLinkageError
+	debug_msg "%s: %s", ecx, ebx
+	invoke_cdecl _free, ebx
+
+.good_link:
+	mov eax, %$LinkStatus
+
+	FrameEnd
+	ret
+
+; GLuint ProgramCreate(size_t num_shaders, int *shaders);
+DefFunc _ProgramCreate
+	FrameBegin ebx, esi, edi
+	NameParams %$NumShaders, %$Shaders
+
+	invoke_stdcall glCreateProgram
+	mov ebx, eax
+
+	mov esi, %$Shaders
+	xor edi, edi
+.attach_shaders:
 	lodsd
 	test eax, eax
-	jz .next_shader
-	invoke_cdecl _ShaderCreate, ebx, %$ShaderType, eax, & %$InfoLog
-	test eax, eax
-	jnz .next_shader
-	GetAbsAddr ecx, _ShaderCompileError
-	debug_msg "%s: %s Shader: %s", ecx, %$ShaderTypeStr, %$InfoLog
-	jmp .bad_end
-.proceed_link:
+	jz .skipped
+	invoke_stdcall glAttachShader, ebx, eax
+.skipped:
+	inc edi
+	cmp edi, %$NumShaders
+	jb .attach_shaders
 
-	invoke_stdcall glLinkProgram, ebx
-	invoke_stdcall glGetProgramiv, ebx, GL_LINK_STATUS, &%$LinkStatus
-	mov eax, %$LinkStatus
+	invoke_cdecl _ProgramLink, ebx
 	test eax, eax
 	jnz .good_link
 
-	invoke_stdcall glGetProgramiv, ebx, GL_INFO_LOG_LENGTH, &%$InfoLogLen
-	mov eax, %$InfoLogLen
-	inc eax
-	invoke_cdecl _calloc, eax, 1
-	mov %$InfoLog, eax
-
-	invoke_stdcall glGetProgramInfoLog, ebx, %$InfoLogLen, &%$InfoLogLen, %$InfoLog
-
-	GetAbsAddr ecx, _ShaderLinkageError
-	debug_msg "%s: %s", ecx, %$InfoLog
-	jmp .bad_end
-
 .bad_end:
-	invoke_cdecl _free, %$InfoLog
 	invoke_stdcall glDeleteProgram, ebx
 	xor ebx, ebx
 
 .good_link:
 	mov eax, ebx
 
+	FrameEnd
+	ret
+
+; void _SceneOnTreeNodeFree(int shader_obj);
+DefFunc _SceneOnTreeNodeFree
+	FrameBegin
+	NameParams %$ShaderObj
+	invoke_stdcall glDeleteShader, %$ShaderObj
+	FrameEnd
+	ret
+
+; GLuint SceneQueryShader(int shader_type, const char *shader_asset_path)
+DefFunc _SceneQueryShader
+	FrameBegin ebx
+	NameParams %$ShaderType, %$ShaderAssetPath
+	DefVars %$ShaderObj
+	GetAbsAddr ebx, _ShadersTree
+
+	invoke_cdecl strlen, %$ShaderAssetPath
+	test eax, eax
+	jz .end
+	invoke_cdecl _AVLSearch, [ebx], %$ShaderAssetPath
+	test eax, eax
+	jnz .found
+	invoke_cdecl _AssetsQuery, %$ShaderAssetPath, 0
+	test eax, eax
+	jz .not_found
+	invoke_cdecl _ShaderCreate, %$ShaderType, eax
+	test eax, eax
+	jz .fail_exit
+	mov %$ShaderObj, eax
+
+	invoke_cdecl _Get_AVLOps_String
+	GetAbsAddr ecx, _SceneOnTreeNodeFree
+	invoke_cdecl _AVLInsert, ebx, %$ShaderAssetPath, %$ShaderObj, ecx, eax
+	jmp .found
+.not_found:
+	invoke_cdecl _GetShaderTypeString, %$ShaderType
+	debug_msg "%s Shader not found: %s", eax, %$ShaderAssetPath
+.fail_exit:
+	xor eax, eax
+	jmp .end
+.found:
+	mov eax, [eax + AVLBST_Node.userdata]
 .end:
+	FrameEnd
+	ret
+
+; void SceneOnDisposeShaders()
+DefFunc _SceneOnDisposeShaders
+	FrameBegin ebx
+	GetAbsAddr ebx, _ShadersTree
+	invoke_cdecl _AVLClear, ebx
 	FrameEnd
 	ret
 
 ; void SceneLoadShaderProgram(_out_ GLuint *program, _in_ char *VertexShaderAssetPath, _in_ char *GeometryShaderAssetPath, _in_ char *FragmentShaderAssetPath);
 DefFunc _SceneLoadShaderProgram
-	FrameBegin esi
+	FrameBegin ebx, esi
 	NameParams %$PProgramOut, %$VSPath, %$GSPath, %$FSPath
-	DefVars %$VSString, %$GSString, %$FSString
+	DefVars %$VSObj, %$GSObj, %$FSObj
+
+	invoke_stdcall glCreateProgram
+	mov ebx, eax
 	mov esi, %$PProgramOut
-	invoke_cdecl _AssetsQuery, %$VSPath, 0
-	mov %$VSString, eax
-	invoke_cdecl _AssetsQuery, %$GSPath, 0
-	mov %$GSString, eax
-	invoke_cdecl _AssetsQuery, %$FSPath, 0
-	mov %$FSString, eax
-	invoke_cdecl _ProgramCreate, %$VSString, %$GSString, %$FSString
+	invoke_cdecl _SceneQueryShader, GL_VERTEX_SHADER, %$VSPath
+	mov %$VSObj, eax
+	invoke_cdecl _SceneQueryShader, GL_GEOMETRY_SHADER, %$GSPath
+	mov %$GSObj, eax
+	invoke_cdecl _SceneQueryShader, GL_FRAGMENT_SHADER, %$FSPath
+	mov %$FSObj, eax
+	invoke_cdecl _ProgramCreate, 3, & %$VSObj
 	mov [esi], eax
 
 	FrameEnd
 	ret
 
-; void SceneLoadShaderProgramEx(_out_ GLuint *program, int shader_type, const char *shader_source, ...);
+; void SceneLoadShaderProgramEx(_out_ GLuint *program, int shader_type, const char *shader_asset_path, ...);
 DefFunc _SceneLoadShaderProgramEx
-	FrameBegin ebx, esi
+	FrameBegin ebx, esi, edi
 	NameParams %$PProgramOut, %$Args
+	DefVars %$ShaderType, %$NumShaders
 
+	xor eax, eax
 	lea esi, %$Args
+	mov %$NumShaders, eax
 	mov ebx, %$PProgramOut
 	mov edi, esi
 .loop_translate_path:
 	lodsd
 	test eax, eax
 	jz .proceed_compile
+	mov %$ShaderType, eax
 	lodsd
 	test eax, eax
 	jz .loop_translate_path
-	invoke_cdecl _AssetsQuery, eax, 0
-	mov [esi - 4], eax
+	invoke_cdecl _SceneQueryShader, %$ShaderType, eax
+	stosd
+	inc dword %$NumShaders
 	jmp .loop_translate_path
 .proceed_compile:
 
-	invoke_cdecl _ProgramCreateExV, & %$Args
+	invoke_cdecl _ProgramCreate, %$NumShaders, & %$Args
 	mov [ebx], eax
 
 	FrameEnd
